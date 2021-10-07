06e79187a80af00beb02bc455970a0bf7e604b50
Alexandre <alexandre.silva@thoughtworks.com>
2021-10-07 12:02:09 -0300

    ajuste ci

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,6 @@
 Cypress.Commands.add('enterPassword', (password, confpassword) =>{
     password = Cypress.env('password')
     confpassword = Cypress.env('confpassword')
-    //Preencher campo Senha
     cy.get('#password').type(password, {log: false})
-    //Prencher campo Confirmar senha
     cy.get('#passwordConfirm').type(confpassword, {log: false})
     })
