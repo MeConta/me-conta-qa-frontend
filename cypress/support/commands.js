fa4cb963c23c2364ba6092752faabfb7d565776f
Alexandre <alexandre.silva@thoughtworks.com>
2021-10-08 16:21:57 -0300

    ajust ci script

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('enterPassword', (password, confpassword) =>{
     password = Cypress.env('CYPRESS_PASSWORD')
-    confpassword = Cypress.env('CYPRESS_CONFPASSWORD')
+    confpassword = Cypress.env('CYPRESS_PASSWORD')
     cy.get('#password').type(password, {log: false})
     cy.get('#passwordConfirm').type(confpassword, {log: false})
     })
