537ce217bf5de64ce4bb9c35daefefe4021dd862
barbaraest <barbara.esteves@thoughtworks.com>
2021-10-21 11:31:42 -0300

    Adiciona preenchimento de campos de cadastro de usuario

--- cypress/integration/Me-Conta/tests/cadastro-inicial/cadastrarAluno.spec.js
+++ cypress/integration/Me-Conta/tests/cadastro-inicial/cadastrarAluno.spec.js
@@ -8,10 +8,11 @@ describe('Cadastrar Aluno', () => {
         cy.visit("/")
     })
 
-    it.only('Cadastrar Aluno - Sucesso', () => {
+    it('Cadastrar Aluno - Sucesso', () => {
         const name = faker.name.findName()
         const email = faker.internet.email()
         const phone = faker.phone.phoneNumber()
+        const data = '2013-10-10'
         
         cy.get('#name').type(name)
         cy.get('#email').type(email)
@@ -20,15 +21,13 @@ describe('Cadastrar Aluno', () => {
         cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.disabled')
         cy.get('#termsConfirm').click()
         cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.enabled').click()
-        cy.wait(3000)
         cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/cadastro-aluno')
         cy.get('.Toastify__toast-body').should('have.text', "O registro foi criado com sucesso!A operação foi realizada com sucesso!")
         cy.get('[data-testid=phone-number]').type(phone)
-        cy.get('#dataNascimento').type('20102019')
-        cy.wait(2000)
-        cy.get('#UF').should('have.value', 'Acre')
+        cy.get('#dataNascimento').should('to.be.visible').type(data , { force: true })
+        cy.get('#UF').should('to.be.visible').select('Acre').should('have.value', 'AC')
         cy.get('#cidade').type('Rio Branco')
-        cy.get('#escolaridade').type('1º Ano - Ensino Médio')
+        cy.get('#escolaridade').should('to.be.visible').select('1').should('have.value', '1')
         cy.get('.styles__Wrapper-sc-s8fbhq-0').click()
         cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/dashboard-aluno')
     })
