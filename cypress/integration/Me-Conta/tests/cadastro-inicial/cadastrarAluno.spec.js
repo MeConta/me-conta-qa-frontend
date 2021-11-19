0f8e1f22fe53df8e1f4ef3570a3bb70c2c406f67
Camila Puchta <camila.puchta@thoughtworks.com>
2021-11-19 15:39:28 -0300

    [Cadastro Aluna] Adicionando testes para campos inválidos e sucesso apenas com campos obrigatórios

--- cypress/integration/Me-Conta/tests/cadastro-inicial/cadastrarAluno.spec.js
+++ cypress/integration/Me-Conta/tests/cadastro-inicial/cadastrarAluno.spec.js
@@ -8,7 +8,7 @@ describe('Cadastrar Aluno', () => {
         cy.visit("/")
     })
 
-    it('Cadastrar Aluno - Sucesso', () => {
+    it('Cadastrar Aluno - Sucesso com todos os campos', () => {
         const name = faker.name.findName()
         const email = faker.internet.email()
         const phone = faker.phone.phoneNumber()
@@ -32,6 +32,31 @@ describe('Cadastrar Aluno', () => {
         cy.get('#necessidades').type('minhas necessidades')
         cy.get('#expectativas').type('minhas expectativas')
         cy.get('#tratamentos').type('meus tratamentos')
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').click()
+        cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/dashboard-aluno')
+    })
+
+    it.skip('Cadastrar Aluno - Sucesso apenas com campos obrigatórios', () => {
+        const name = faker.name.findName()
+        const email = faker.internet.email()
+        const phone = faker.phone.phoneNumber()
+        const data = '2013-10-10'
+        
+        cy.get('#name').type(name)
+        cy.get('#email').type(email)
+        cy.enterPassword(Cypress.env('CYPRESS_PASSWORD'), Cypress.env('CYPRESS_PASSWORD'))
+        cy.get("[value='0']").should('to.be.checked') 
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.disabled')
+        cy.get('#termsConfirm').click()
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.enabled').click()
+        cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/cadastro-aluno')
+        cy.get('#telefone').type(phone)
+        cy.get('#dataNascimento').should('to.be.visible').type(data , { force: true })
+        cy.get('#UF').should('to.be.visible').select('Acre').should('have.value', 'AC')
+        cy.get('#cidade').type('Rio Branco')
+        cy.get('#Feminino').should('to.be.visible').click()
+        cy.get('#escolaridade').should('to.be.visible').select('1').should('have.value', '1')
+        cy.get('[name=tipoEscola]').should('to.be.checked')
         cy.get('.styles__Wrapper-sc-s8fbhq-0').click()
         cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/dashboard-aluno')
     })
@@ -70,5 +95,24 @@ describe('Cadastrar Aluno', () => {
         cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/cadastro-aluno')
         cy.get('.styles__Link-sc-xhmkr6-2 > a').click()
         cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/dashboard-aluno')
+    })
+
+    it('Cadastrar Aluno - Campos inválidos', () => {
+        const name = faker.name.findName()
+        const email = faker.internet.email()
+
+        cy.get('#name').type(name)
+        cy.get('#email').type(email)
+        cy.enterPassword(Cypress.env('CYPRESS_PASSWORD'), Cypress.env('CYPRESS_PASSWORD'))
+        cy.get("[value='0']").should('to.be.checked') 
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.disabled')
+        cy.get('#termsConfirm').click()
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.enabled').click()
+        cy.url().should('eq', 'https://me-conta-frontend.herokuapp.com/cadastro-aluno')
+        cy.get('#dataNascimento').type('1111-11-11')
+        cy.get('#cidade').type('Aa')
+        cy.get('.styles__Wrapper-sc-s8fbhq-0').should('to.be.disabled')
+        cy.get(':nth-child(4) > .styles__Error-sc-1xrqtb1-3').should('have.text', " Data de nascimento inválida. ")
+        cy.get(':nth-child(6) > .styles__Error-sc-1xrqtb1-3').should('have.text', " Cidade é obrigatório. ")
     })
 })
