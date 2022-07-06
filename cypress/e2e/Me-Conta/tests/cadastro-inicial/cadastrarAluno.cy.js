1da56dbc992075e06d3eb93a84dfb70e10ba05ad
Flavia Souza <flavias.git@gmail.com>
2022-07-06 17:51:31 -0300

    test: updating cypress to version 10.3.0

---
+++ cypress/e2e/Me-Conta/tests/cadastro-inicial/cadastrarAluno.cy.js
@@ -0,0 +1,69 @@
+/// <reference types="Cypress" />
+
+import faker from "@faker-js/faker";
+faker.setLocale("pt_BR");
+
+describe("Cadastrar Aluno", () => {
+  beforeEach(() => {
+    const name = faker.name.findName();
+    const email = faker.internet.email();
+
+    cy.criarConta(name, email, 0);
+    cy.url().should("include", "/criar-conta");
+  });
+
+  it("Cadastrar Aluno - Sucesso com todos os campos", () => {
+    const phone = faker.phone.phoneNumber("###########");
+    const data = "2013-10-10";
+
+    cy.get("#telefone").type(phone);
+    cy.get("#dataNascimento").type(data, { force: true });
+    cy.get("#UF").select("Acre").should("have.value", "AC");
+    cy.get("#cidade").type("Rio Branco");
+    cy.get("#Feminino").click();
+    cy.get("#Escolaridade")
+      .select("1º Ano - Ensino Médio")
+      .should("have.value", "1");
+    cy.get("[name=tipoEscola]").should("to.be.checked");
+    cy.get("#necessidades").type("minhas necessidades");
+    cy.get("#expectativas").type("minhas expectativas");
+    cy.get("#tratamentos").type("meus tratamentos");
+    cy.contains("Concluir minha inscrição").click();
+    cy.url().should("include", "/dashboard-aluno");
+  });
+
+  it("Cadastrar Aluno - Sucesso apenas com campos obrigatórios", () => {
+    const phone = faker.phone.phoneNumber("###########");
+    const data = "2013-10-10";
+
+    cy.get("#escolaridade").select("1").should("have.value", "1");
+    cy.get("#telefone").type(phone);
+    cy.get("#dataNascimento").type(data, { force: true });
+    cy.get("#UF").select("Acre").should("have.value", "AC");
+    cy.get("#cidade").type("Rio Branco");
+    cy.get("#Feminino").click();
+    cy.get("[name=tipoEscola]").should("to.be.checked");
+    cy.contains("Concluir minha inscrição").click();
+    cy.url().should("include", "/dashboard-aluno");
+  });
+
+  it("Cadastrar Aluno - Campos vazios", () => {
+    cy.contains("Concluir minha inscrição").click();
+    cy.get('[data-testid="Telefone"]')
+      .parent()
+      .contains("Telefone é obrigatório");
+    cy.get('[data-testid="Data de nascimento"]')
+      .parent()
+      .contains("Data de nascimento inválida");
+    cy.get('[data-testid="Estado"]').parent().contains("Estado é obrigatório");
+    cy.get('[data-testid="Cidade"]').parent().contains("Cidade é obrigatório");
+    cy.get('[data-testid="Escolaridade"]')
+      .parent()
+      .contains("Escolaridade é obrigatória");
+  });
+
+  it("Cadastrar Aluno - Pular preenchimento de dados", () => {
+    cy.contains("Preencher depois").click();
+    cy.url().should("include", "/dashboard-aluno");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,17 @@
-Cypress.Commands.add('enterPassword', (password, confpassword) => {
-    cy.get('#password').type(password, { log: false })
-    cy.get('#passwordConfirm').type(confpassword, { log: false })
-})
+Cypress.Commands.add("enterPassword", (password, confpassword) => {
+  cy.get("#password").type(password, { log: false });
+  cy.get("#passwordConfirm").type(confpassword, { log: false });
+});
 
-Cypress.Commands.add('criarConta', (name, email, tipo) => {
-    const password = Cypress.env('PASSWORD')
-    cy.visit('/criar-conta')
-    cy.get('#name').type(name)
-    cy.get('#email').type(email)
-    cy.enterPassword(password, password)
-    // Verify that default is checked
-    cy.get("[value='0']").should('to.be.checked')
-    cy.get(`[value=${tipo}]`).click()
-    cy.get('#termsConfirm').click()
-    cy.contains('CADASTRAR').click()
-})
+Cypress.Commands.add("criarConta", (name, email, tipo) => {
+  const password = Cypress.env("PASSWORD");
+  cy.visit("/criar-conta");
+  cy.get("#name").type(name);
+  cy.get("#email").type(email);
+  cy.enterPassword(password, password);
+  // Verify that default is checked
+  cy.get("[value='0']").should("to.be.checked");
+  cy.get(`[value=${tipo}]`).click();
+  cy.get("#termsConfirm").click();
+  cy.contains(/CADASTRAR/i).click();
+});
