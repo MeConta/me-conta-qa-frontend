e51c81bc680d022c2691bdc1f1dba0a7cc4575d6
Vanessa Furtado de Lima <vanessalima@MacBook-Pro-de-Vanessa.local>
2022-07-18 17:59:16 -0300

    test(#151): adding successfull account registration tests

--- cypress/e2e/Me-Conta/tests/cadastro-inicial/criarContaAluno.cy.js
+++ cypress/e2e/Me-Conta/tests/cadastro-inicial/criarContaAluno.cy.js
@@ -1,3 +1,6 @@
+import faker from "@faker-js/faker";
+faker.setLocale("pt_BR");
+
 context("funcionalidade: acessar a página de cadastro", () => {
   before(() => {
     cy.visit("/login");
@@ -20,25 +23,59 @@ context("funcionalidade: acessar a página de cadastro", () => {
   });
 
   describe("Então: sou redirecionado para a primeira tela de cadastro", () => {
-    it("eu vejo a primeira tela do cadastro", () => {
+    it("eu sou redirecionado a primeira tela do cadastro", () => {
       cy.url().should("include", "/criar-conta");
-      cy.contains(/Criar Conta/).should("be.visible");
     });
   });
 });
 
 context("funcionalidade: completar detalhes da conta", () => {
-  describe("Dado: que estou na primeira tela do formulário de cadastro", () => {});
+  const password = Cypress.env("PASSWORD");
+  const name = "Maria Silva Só";
+  const email = faker.internet.email();
 
-  describe("Quando: eu preencho todos os dados", () => {});
+  describe("Dado: que estou na primeira tela do formulário de cadastro", () => {
+    it("eu vejo a primeira tela do cadastro", () => {
+      cy.contains(/Criar Conta/i).should("be.visible");
+    })
+  });
 
-  describe('E: escolho o tipo de usuário "Aluno"', () => {});
+  describe("Quando: eu preencho todos os dados", () => {
+    it("eu preencho todos os campos do formulário (nome, email e senha)", () => {
+      cy.get("#name").type(name);
+      cy.get("#email").type(email);
+      cy.enterPassword(password, password);
+    })
+  });
 
-  describe("E: aceito os termos de uso e política de privacidade", () => {});
+  describe('E: escolho o tipo de usuário "Aluno"', () => {
+    it("eu seleciono o usuário do tipo Aluno", () => {
+      const aluno = '0';
+      cy.get(`[value=${aluno}]`).click();
+      cy.get(`[value=${aluno}]`).should("to.be.checked");
+    })
+  });
 
-  describe("E: clico no botão de Cadastrar", () => {});
+  describe("E: aceito os termos de uso e política de privacidade", () => {
+    it("eu seleciono que aceito os termos de uso", () => {
+      cy.get("[name=termsConfirm]").click();
+      cy.get("[name=termsConfirm]").should("to.be.checked");
+    })
+  });
 
-  describe("Então: sou redirecionado para a segunda tela de cadastro do aluno (Dados Pessoais)", () => {});
+  describe("E: clico no botão de Cadastrar", () => {
+    it("eu clico no botão de Cadastrar", () => {
+      const cadastrarButton = cy.contains(/Cadastrar/i);
+      cadastrarButton.should("be.enabled");
+      cadastrarButton.click();
+    })
+  });
+
+  describe("Então: sou redirecionado para a segunda tela de cadastro do aluno (Dados Pessoais)", () => {
+    it("eu vejo a segunda tela de cadastro (dados pessoais)", () => {
+      cy.contains(/Complete seus Dados Pessoais/i).should("be.visible");
+    })
+  });
 });
 
 context(
