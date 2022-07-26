2c46432d7ff7ce660af8f3a1805a8806a0b0246a
Manuella Vieira <manuella.vieira@thoughtworks.com>
2022-07-26 17:45:40 -0300

    test(#151): disabling database cleanup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,26 +17,32 @@
  */
 // eslint-disable-next-line no-unused-vars
 
-const { cleanUpDatabase } = require("./utils/db")
-require('dotenv/config');
+const { cleanUpDatabase } = require("./utils/db");
+require("dotenv/config");
+
+const toggleDatabaseCleanup = false;
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('before:run', async () => {
-    // In localhost we need ssl == false, running in heroku/github use rejectUnauthorized instead.
-    const ssl = process.env.PGSSL ? (process.env.PGSSL === 'true') : { rejectUnauthorized: false };
-    const options = {
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      password: process.env.PGPASSWORD,
-      port: process.env.PGPORT,
-      dialect: "postgres",
-      ssl: ssl,
+  on("before:run", async () => {
+    if (toggleDatabaseCleanup) {
+      // In localhost we need ssl == false, running in heroku/github use rejectUnauthorized instead.
+      const ssl = process.env.PGSSL
+        ? process.env.PGSSL === "true"
+        : { rejectUnauthorized: false };
+      const options = {
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: process.env.PGPORT,
+        dialect: "postgres",
+        ssl: ssl,
+      };
+      console.log("Starting database clean up");
+      await cleanUpDatabase(options);
+      console.log("Database clean up finished");
     }
-    console.log('Starting database clean up');
-    await cleanUpDatabase(options);
-    console.log('Database clean up finished');
-  })
-}
+  });
+};
